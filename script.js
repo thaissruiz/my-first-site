d768f38a9e42fc52a0a936c84e9761b33864d642
thaissruiz <thaaaisxd@gmail.com>
2021-04-06 20:30:09 -0300

    atualizado

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ window.onload = function() {
     var button = document.querySelector('button');
   
     button.addEventListener('click', function() {
-      alert("Let's Rock!!");
+      alert(" I  can be better every day");
     });
   }
 
   //Botao que mostra mensagem
-  
